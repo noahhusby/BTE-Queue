ca33b039643577fa88aaf7890ed00ec432c58c7d
unknown <noahhusby@gmail.com>
2020-08-31 16:35:58 -0400

    Updated to the MC Protocol

--- README.md
+++
@@ -1,2 +0,0 @@
-# BTE-Queue
-A queue for the official Build The Earth network
--- src/main/java/com/noahhusby/btequeue/BTEQueue.java
+++ src/main/java/com/noahhusby/btequeue/BTEQueue.java
@@ -1,30 +1,211 @@
 package com.noahhusby.btequeue;
 
-import com.noahhusby.btequeue.util.ServerRule;
-import org.bukkit.Difficulty;
-import org.bukkit.GameMode;
-import org.bukkit.event.Listener;
-import org.bukkit.plugin.java.JavaPlugin;
+import com.github.steveice10.mc.auth.data.GameProfile;
+import com.github.steveice10.mc.auth.exception.request.RequestException;
+import com.github.steveice10.mc.protocol.MinecraftConstants;
+import com.github.steveice10.mc.protocol.MinecraftProtocol;
+import com.github.steveice10.mc.protocol.ServerLoginHandler;
+import com.github.steveice10.mc.protocol.data.SubProtocol;
+import com.github.steveice10.mc.protocol.data.game.chunk.BlockStorage;
+import com.github.steveice10.mc.protocol.data.game.chunk.Chunk;
+import com.github.steveice10.mc.protocol.data.game.chunk.Column;
+import com.github.steveice10.mc.protocol.data.game.chunk.NibbleArray3d;
+import com.github.steveice10.mc.protocol.data.game.entity.player.GameMode;
+import com.github.steveice10.mc.protocol.data.game.setting.Difficulty;
+import com.github.steveice10.mc.protocol.data.game.world.WorldType;
+import com.github.steveice10.mc.protocol.data.game.world.block.BlockState;
+import com.github.steveice10.mc.protocol.data.message.*;
+import com.github.steveice10.mc.protocol.data.status.PlayerInfo;
+import com.github.steveice10.mc.protocol.data.status.ServerStatusInfo;
+import com.github.steveice10.mc.protocol.data.status.VersionInfo;
+import com.github.steveice10.mc.protocol.data.status.handler.ServerInfoBuilder;
+import com.github.steveice10.mc.protocol.data.status.handler.ServerInfoHandler;
+import com.github.steveice10.mc.protocol.data.status.handler.ServerPingTimeHandler;
+import com.github.steveice10.mc.protocol.packet.ingame.client.ClientChatPacket;
+import com.github.steveice10.mc.protocol.packet.ingame.client.ClientKeepAlivePacket;
+import com.github.steveice10.mc.protocol.packet.ingame.server.ServerChatPacket;
+import com.github.steveice10.mc.protocol.packet.ingame.server.ServerJoinGamePacket;
+import com.github.steveice10.mc.protocol.packet.ingame.server.world.ServerChunkDataPacket;
+import com.github.steveice10.mc.protocol.packet.login.client.LoginStartPacket;
+import com.github.steveice10.packetlib.Client;
+import com.github.steveice10.packetlib.Server;
+import com.github.steveice10.packetlib.Session;
+import com.github.steveice10.packetlib.event.server.ServerAdapter;
+import com.github.steveice10.packetlib.event.server.SessionAddedEvent;
+import com.github.steveice10.packetlib.event.server.SessionRemovedEvent;
+import com.github.steveice10.packetlib.event.session.DisconnectedEvent;
+import com.github.steveice10.packetlib.event.session.PacketReceivedEvent;
+import com.github.steveice10.packetlib.event.session.SessionAdapter;
+import com.github.steveice10.packetlib.tcp.TcpSessionFactory;
 
-public final class BTEQueue extends JavaPlugin implements Listener {
+import javax.swing.*;
+import java.net.Proxy;
+import java.util.Arrays;
 
-    @Override
-    public void onEnable() {
-        getServer().getPluginManager().registerEvents(new QueueRestrictions(), this);
-        configureServer();
+public class BTEQueue {
+    private static final boolean SPAWN_SERVER = true;
+    private static final boolean VERIFY_USERS = false;
+    private static final String HOST = "127.0.0.1";
+    private static final int PORT = 25565;
+    private static final Proxy PROXY = Proxy.NO_PROXY;
+    private static final Proxy AUTH_PROXY = Proxy.NO_PROXY;
+    private static final String USERNAME = "Username";
+    private static final String PASSWORD = "Password";
+
+    public static void main(String[] args) {
+        if(SPAWN_SERVER) {
+            Server server = new Server(HOST, PORT, MinecraftProtocol.class, new TcpSessionFactory(PROXY));
+            server.setGlobalFlag(MinecraftConstants.AUTH_PROXY_KEY, AUTH_PROXY);
+            server.setGlobalFlag(MinecraftConstants.VERIFY_USERS_KEY, VERIFY_USERS);
+            server.setGlobalFlag(MinecraftConstants.SERVER_INFO_BUILDER_KEY, new ServerInfoBuilder() {
+                @Override
+                public ServerStatusInfo buildInfo(Session session) {
+                    return new ServerStatusInfo(new VersionInfo(MinecraftConstants.GAME_VERSION, MinecraftConstants.PROTOCOL_VERSION), new PlayerInfo(100, 0, new GameProfile[0]), new TextMessage("Hello world!"), null);
+                }
+            });
+
+            server.setGlobalFlag(MinecraftConstants.SERVER_LOGIN_HANDLER_KEY, new ServerLoginHandler() {
+                @Override
+                public void loggedIn(Session session) {
+                    session.send(new ServerJoinGamePacket(0, false, GameMode.CREATIVE, 0, Difficulty.PEACEFUL, 10, WorldType.DEFAULT, false));
+                        BlockStorage b = new BlockStorage();
+                        System.out.println("1!");
+                        for(int x = 0; x < 16; x++) {
+                            for(int y = 0; y < 16; y++) {
+                                for(int z = 0; z < 16; z++) {
+                                    b.set(x, y, z, new BlockState(98, 0));
+                                }
+                            }
+                        }
+                        System.out.println("2!");
+                        Chunk chk = new Chunk(b,  new NibbleArray3d(4096), new NibbleArray3d(4096));
+                        System.out.println("3!");
+                        for(int x = 0; x < 4; x++) {
+                            for(int z = 0; z < 4; z++) {
+                                Column col = new Column(x, z, new Chunk[] { chk , chk , chk, chk, chk, chk, chk, chk, chk , chk , chk, chk, chk, chk, chk, chk }, new byte[256], null);
+                                ServerChunkDataPacket pkX = new ServerChunkDataPacket(col);
+                                session.send(pkX);
+
+                                col = new Column(-x, z, new Chunk[] { chk , chk , chk, chk, chk, chk, chk, chk, chk , chk , chk, chk, chk, chk, chk, chk }, new byte[256], null);
+                                pkX = new ServerChunkDataPacket(col);
+                                //session.send(pkX);
+
+                                col = new Column(-x, -z, new Chunk[] { chk , chk , chk, chk, chk, chk, chk, chk, chk , chk , chk, chk, chk, chk, chk, chk }, new byte[256], null);
+                                pkX = new ServerChunkDataPacket(col);
+                                //session.send(pkX);
+
+                                col = new Column(x, -z, new Chunk[] { chk , chk , chk, chk, chk, chk, chk, chk, chk , chk , chk, chk, chk, chk, chk, chk }, new byte[256], null);
+                                pkX = new ServerChunkDataPacket(col);
+                                //session.send(pkX);
+                            }
+                        }
+
+                }
+            });
+
+            server.setGlobalFlag(MinecraftConstants.SERVER_COMPRESSION_THRESHOLD, 100);
+            server.addListener(new ServerAdapter() {
+                @Override
+                public void sessionAdded(SessionAddedEvent event) {
+                    event.getSession().addListener(new SessionAdapter() {
+                        @Override
+                        public void packetReceived(PacketReceivedEvent event) {
+                            if(event.getPacket() instanceof ClientKeepAlivePacket) {
+                                ClientKeepAlivePacket clientKeepAlivePacket = event.getPacket();
+                            }
+                        }
+                    });
+                }
+
+                @Override
+                public void sessionRemoved(SessionRemovedEvent event) {
+                    MinecraftProtocol protocol = (MinecraftProtocol) event.getSession().getPacketProtocol();
+                    if(protocol.getSubProtocol() == SubProtocol.GAME) {
+                    }
+                }
+            });
+
+            server.bind();
+        }
+
+        status();
+        //login();
     }
 
-    public void configureServer() {
-        getServer().setDefaultGameMode(GameMode.SURVIVAL);
-        ServerRule.setGamerule("announceAdvancements", "false");
-        ServerRule.setGamerule("doDaylightCycle", "false");
-        ServerRule.setGamerule("doFireTick", "false");
-        ServerRule.setGamerule("doMobSpawning", "false");
-        ServerRule.setGamerule("doWeatherCycle", "false");
-        ServerRule.setGamerule("mobGriefing", "false");
-        ServerRule.setGamerule("showDeathMessages", "false");
-        ServerRule.setDifficulty(Difficulty.PEACEFUL);
-        ServerRule.setTime(15000);
+    private static void status() {
+        MinecraftProtocol protocol = new MinecraftProtocol(SubProtocol.STATUS);
+        Client client = new Client(HOST, PORT, protocol, new TcpSessionFactory(PROXY));
+        client.getSession().setFlag(MinecraftConstants.AUTH_PROXY_KEY, AUTH_PROXY);
+        client.getSession().setFlag(MinecraftConstants.SERVER_INFO_HANDLER_KEY, new ServerInfoHandler() {
+            @Override
+            public void handle(Session session, ServerStatusInfo info) {
+                System.out.println("Version: " + info.getVersionInfo().getVersionName() + ", " + info.getVersionInfo().getProtocolVersion());
+                System.out.println("Player Count: " + info.getPlayerInfo().getOnlinePlayers() + " / " + info.getPlayerInfo().getMaxPlayers());
+                System.out.println("Players: " + Arrays.toString(info.getPlayerInfo().getPlayers()));
+                System.out.println("Description: " + info.getDescription().getFullText());
+                System.out.println("Icon: " + info.getIcon());
+            }
+        });
+
+        client.getSession().setFlag(MinecraftConstants.SERVER_PING_TIME_HANDLER_KEY, new ServerPingTimeHandler() {
+            @Override
+            public void handle(Session session, long pingTime) {
+                System.out.println("Server ping took " + pingTime + "ms");
+            }
+        });
+
+        client.getSession().connect();
+        while(client.getSession().isConnected()) {
+            try {
+                Thread.sleep(5);
+            } catch(InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void login() {
+        MinecraftProtocol protocol = null;
+        if(VERIFY_USERS) {
+            try {
+                protocol = new MinecraftProtocol(USERNAME, PASSWORD, false);
+                System.out.println("Successfully authenticated user.");
+            } catch(RequestException e) {
+                e.printStackTrace();
+                return;
+            }
+        } else {
+            protocol = new MinecraftProtocol(USERNAME);
+        }
+
+        Client client = new Client(HOST, PORT, protocol, new TcpSessionFactory(PROXY));
+        client.getSession().setFlag(MinecraftConstants.AUTH_PROXY_KEY, AUTH_PROXY);
+        client.getSession().addListener(new SessionAdapter() {
+            @Override
+            public void packetReceived(PacketReceivedEvent event) {
+                if(event.getPacket() instanceof ServerJoinGamePacket) {
+                    event.getSession().send(new ClientChatPacket("Hello, this is a test of MCProtocolLib."));
+                } else if(event.getPacket() instanceof ServerChatPacket) {
+                    Message message = event.<ServerChatPacket>getPacket().getMessage();
+                    System.out.println("Received Message: " + message.getFullText());
+                    if(message instanceof TranslationMessage) {
+                        System.out.println("Received Translation Components: " + Arrays.toString(((TranslationMessage) message).getTranslationParams()));
+                    }
+
+                    event.getSession().disconnect("Finished");
+                }
+            }
+
+            @Override
+            public void disconnected(DisconnectedEvent event) {
+                System.out.println("Disconnected: " + Message.fromString(event.getReason()).getFullText());
+                if(event.getCause() != null) {
+                    event.getCause().printStackTrace();
+                }
+            }
+        });
+
+        client.getSession().connect();
     }
 
 }
